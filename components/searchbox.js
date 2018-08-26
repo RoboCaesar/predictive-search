a01eeb2f38be934a8642bb37c1413da8dbd1fc27
Thomas Draxler <twdrax@gmail.com>
2018-08-26 19:03:21 +0700

    A search results box now appears when the user starts typing.

--- components/searchbox.js
+++ components/searchbox.js
@@ -2,9 +2,15 @@ class SearchBox extends React.Component {
     constructor(props) {
         super(props);
         this.handleEntry = this.handleEntry.bind(this);
+        this.state = {
+            resultsVisible: false
+        }
     }
 
-    handleEntry() {
+    handleEntry(e) {
+        this.setState((prevState) => {
+            return {resultsVisible: true}
+        });
         e.preventDefault();
     }
 
@@ -12,8 +18,11 @@ class SearchBox extends React.Component {
         return (
             <div>
                 <form>
-                    <input type="text" />
+                    <input onInput={this.handleEntry} type="text" />
                 </form>
+                <div className="results-box" style={{display: this.state.resultsVisible ? 'block' : 'none'}}>
+                    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+                </div>
             </div>
         );
     }
